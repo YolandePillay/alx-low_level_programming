d0fee2fe29a84755eff4ef02ace2f05b5b3b0240
Yolande Pillay <yolande.govender@gmail.com>
2023-04-20 05:29:01 -0700

    C hello World updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - print a line using puts
  *
  * Return: 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like buildinga multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
